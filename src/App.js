8fa45336e92aa12b4ce2e4cba39e983a2828617d
Meli357 <wyck_f@hotmail.com>
2022-11-09 20:23:22 +0100

    modify App component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ export class App extends React.Component{
         return(
             <div>
                 <Hello />
+                <Hello />
             </div>
         )
     }
 }
+
+// se inserisco l'Hello component più di una volta
+// dentro l'App component viene reso la quantità di 
+// volte che è stato inserito
