adb15d9a1dd48e50e77d23807c6ae5a6a7fbae56
Meli357 <wyck_f@hotmail.com>
2022-11-10 18:45:06 +0100

    add conditional

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component{
     render(){
         return(
             <div>
-                <p>Welcome, {nameGreet}</p>
+                <p>Welcome, {nameGreet}!</p>
                 {this.props.age > 18 && <Age age={this.props.age}/>}
             </div>
         )
@@ -13,4 +13,4 @@ export class Welcome extends React.Component{
 }
 
 const name = "Melissa"
-const nameGreet = <p>Welcome, <strong>{name}</strong>!</p>
+const nameGreet = <strong>{name}</strong>
