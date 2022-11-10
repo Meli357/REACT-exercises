987c4c859e83e5be135f08a50a725f71b9549ae5
Meli357 <wyck_f@hotmail.com>
2022-11-10 20:02:48 +0100

    add answer

--- src/Counter.js
+++ src/Counter.js
@@ -19,3 +19,9 @@ export class Counter extends React.Component{
         return <h1>Counter : {this.state.count}</h1>
     }
 }
+
+// if next value of state doesn't depend on 
+// current value of state use object:
+//     this.setState({count: this.state.count + 1})
+// }, 1000);
+// otherwise use callback as the parameter
