dc1ec1e7f6a3bee57df5eb45b985cc43a0cdf91a
Meli357 <wyck_f@hotmail.com>
2022-11-18 16:34:44 +0100

    add img inside buttons

--- src/ClickTracker.js
+++ src/ClickTracker.js
@@ -7,7 +7,7 @@ export class ClickTracker extends React.Component{
         const newH1 = document.createElement('h1');
         document.body.appendChild(newH1);
 
-        newH1.innerHTML = event.target.innerText;
+        newH1.innerHTML = event.target.alt;
          
         console.log(event.target)
         console.log(newH1)
@@ -16,10 +16,12 @@ export class ClickTracker extends React.Component{
     render(){
         return(
             <div>
-                <button onClick={this.handlerButtons}>First</button>
-                <button onClick={this.handlerButtons}>Second</button>
-                <button onClick={this.handlerButtons}>Third</button>
+                <button onClick={this.handlerButtons}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbP205eYYe4LAehxgRA2q8BgXdeY_LXwYSiw&usqp=CAU" alt="blue button"/></button>
+                <button onClick={this.handlerButtons}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcggZhILFiBD7XhCViH2uWr-t7XZUUdcgyKQ&usqp=CAU" alt="red button"/></button>
+                <button onClick={this.handlerButtons}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX_WUe6xqw_jeDTxT2Eml3D-MhAgCf7NZcEA&usqp=CAU" alt="yellow button"/></button>
             </div>
         )
     }
 }
+
+// we add an alt property inside img so it can be returned by the event handler
