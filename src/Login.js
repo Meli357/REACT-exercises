72f306eda68786b33cdf7ca7fc2ca8d054570cfd
Meli357 <wyck_f@hotmail.com>
2022-11-18 20:37:47 +0100

    add Login component

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export class Login extends React.Component{
+    state={
+        username:'',
+        password:'',
+        remember: false
+    }
+
+    handleAllInputs= (event) =>{
+        const name = event.target.name
+        const value = event.target.value
+        const type = event.target.type
+        const checked = event.target.checked
+        this.setState({
+            [name]:type === 'checkbox' ? checked : value,
+        })
+    }
+
+    componentDidUpdate(){
+        console.log(this.state)
+    }
+
+    render(){
+        return (
+            <div>
+                <h3>Form:</h3>
+                <input name="username" value={this.state.username} onChange={this.handleAllInputs} />
+                <input name="password" value={this.state.password} type="password" onChange={this.handleAllInputs} />
+                <input name="remember" checked={this.state.remember} type="checkbox" onChange={this.handleAllInputs} />
+            </div>
+        )
+    }
+}
