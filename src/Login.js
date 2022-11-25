0a81b01791db69dce6e3264663b3ccbfd4fcba00
Meli357 <wyck_f@hotmail.com>
2022-11-25 17:16:43 +0100

    add inline style to button

--- src/Login.js
+++ src/Login.js
@@ -25,19 +25,29 @@ export class Login extends React.Component{
         })
     }
         
-    onLogin = () =>{
-        console.log(this.state)
+    // onLogin = () =>{
+    //     console.log(this.state)
+    // }
+    onLogin = () => {
+        this.setState({
+            username: this.state.username,
+            password: this.state.password,
+            remember: this.state.remember
+        })
     }
 
     render(){
         const toDisable = this.state.username === '' && this.state.password === '';
+        const StyleBtn ={
+            backgroundColor : this.state.password.length < 8 ? 'red' : 'green',
+        }
         return (
             <div>
                 <h3>Form:</h3>
                 <input name="username" value={this.state.username} onChange={this.handleAllInputs}></input>
                 <input name="password" value={this.state.password} type="password" onChange={this.handleAllInputs}></input>
                 <input name="remember" checked={this.state.remember} type="checkbox" onChange={this.handleAllInputs}></input>
-                <button disabled={toDisable ? true : false} onClick={this.onLogin}>Login</button>
+                <button disabled={toDisable ? true : false} onClick={this.onLogin} style={StyleBtn} >Login</button>
                 <button onClick={this.handleResetBtn}>Reset</button>
             </div>
         )
