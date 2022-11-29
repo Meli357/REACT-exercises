58d7f51c4adda3d519ec86217dda007011e7bd47
Meli357 <wyck_f@hotmail.com>
2022-11-29 02:56:21 +0100

    use LanguageContext.Consumer

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { LanguageContext } from "./Language";
 
+const strings={
+    en: {VERSION:"English version"},
+    it: {VERSION:"Italian version"},
+    es: {VERSION:"Spanish version"},
+}
 export class DisplayLanguage extends React.Component{
     render(){
-        return(<div></div>)
+        return(<div>
+            <LanguageContext.Consumer>
+                {language =>{
+                    return(<h1>
+                        {strings[language].VERSION}
+                    </h1>)
+                }}
+            </LanguageContext.Consumer>
+        </div>)
     }
 }
