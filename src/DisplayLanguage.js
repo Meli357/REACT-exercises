a197ceaf8ddcf6d6f0f53d18b056f2d9d2902be2
Meli357 <wyck_f@hotmail.com>
2022-12-05 02:55:52 +0100

    add useContext hook

--- src/DisplayLanguage.js
+++ src/DisplayLanguage.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { LanguageContext } from "./Language";
+import { useContext } from "react";
 
-const strings={
-    en: {VERSION:"English version"},
-    it: {VERSION:"Italian version"},
-    es: {VERSION:"Spanish version"},
-}
-export class DisplayLanguage extends React.Component{
-    render(){
+// const strings={
+//     en: {VERSION:"English version"},
+//     it: {VERSION:"Italian version"},
+//     es: {VERSION:"Spanish version"},
+// }
+export function DisplayLanguage(){
+    const language = useContext(LanguageContext)
         return(<div>
-            <LanguageContext.Consumer>
+            {/* <LanguageContext.Consumer>
                 {language =>{
                     return(<h1>
                         {strings[language].VERSION}
                     </h1>)
                 }}
-            </LanguageContext.Consumer>
+            </LanguageContext.Consumer> */}
+            <h3>Current language:{language}</h3>
         </div>)
-    }
+    
 }
