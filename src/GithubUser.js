54a38d2bf6c54f3abb8a5b96e4aced0be6f338d3
Meli357 <wyck_f@hotmail.com>
2022-12-07 00:08:40 +0100

    add useSWR hook

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,37 +1,38 @@
-import { useEffect, useState } from 'react'
+import { useGithubUser } from "./useGithubUser"
+// import { useEffect, useState } from 'react'
 
-export function useGithubUser(username){
-    const [data, setData]= useState(null)
-    const [loading,setLoading]= useState(false)
-    const [error, setError]= useState(null)
+// export function useGithubUser(username){
+//     const [data, setData]= useState(null)
+//     const [loading,setLoading]= useState(false)
+//     const [error, setError]= useState(null)
 
-    async function fetchGithubUser(username){
-        setError(null)
-        setLoading(true)
+//     async function fetchGithubUser(username){
+//         setError(null)
+//         setLoading(true)
 
-        try{
-            const response = await fetch(`https://api.github.com/users/${username}`)
-            const json = await response.json()
+//         try{
+//             const response = await fetch(`https://api.github.com/users/${username}`)
+//             const json = await response.json()
             
-            if (response.status!==200){
-                setError(new Error())
-            }
+//             if (response.status!==200){
+//                 setError(new Error())
+//             }
 
-            setData(json)
-        }catch(error){
-            setError(null)
-            setData(null)
-        }finally {
-            setLoading(false)
-        }
-    }
+//             setData(json)
+//         }catch(error){
+//             setError(null)
+//             setData(null)
+//         }finally {
+//             setLoading(false)
+//         }
+//     }
 
-    useEffect(() => {
-        fetchGithubUser(username)
-    },[username])
+//     useEffect(() => {
+//         fetchGithubUser(username)
+//     },[username])
 
-    return {data,loading,error}
-}
+//     return {data,loading,error}
+// }
 
 export function GithubUser({username}){
     const{data,loading,error}= useGithubUser(username)
